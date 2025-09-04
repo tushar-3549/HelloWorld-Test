5402b0236549ae896b310a485b43af46659321c2
tushar-3549 <tushardiu3549@gmail.com>
2025-09-04 15:22:37 +0600

    add postgress db

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -66,12 +66,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hello_world_db',
+        'USER': 'postgres',
+        'PASSWORD': 'tushar3549',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
